b7f585bb8fb53641080a1c7e28d4674304ee5cda
Rajab <rkarume@gmail.com>
2022-08-22 18:35:24 +0300

    commit tree 6ac7a870c0957121e9b4521d76a1f1aa396ac3f2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+document.getElementById('victory').innerText('Rajab is the champion');
